8b4eb52dfa13d5db6f76b1eaf121991d60ebfcb8
KJayaraju <jayarajukolli123@gmail.com>
2025-04-21 11:33:49 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+// src/components/Footer.js
+import React from 'react';
+import './Footer.css';
+import { FaCheckCircle } from 'react-icons/fa';  // Using Font Awesome icon as a logo
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <img className='imgae' src="https://www.pngall.com/wp-content/uploads/5/Checklist-Logo-PNG.png" alt="logo" />
+        <h3 className="footer-title">ᵀ𝑜𝓓ⲟĿ𝔦ᔕț</h3>
+      </div>
+      <p className="footer-text">© 2025 To-Do List. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
